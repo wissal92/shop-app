a14fef5bdefcf0515de572ed528991d8f209ba3d
wissal <wissal.el@gmail.com>
2019-07-10 14:37:28 -0700

    Added routing to our react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () =>{
+  return(
+    <div>
+      <h1>HatS Page (:</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-       <HomePage/>
+       <Switch>
+         <Route exact path='/' component={HomePage}/>
+         <Route exact path='/hats' component={HatsPage}/>
+       </Switch>
     </div>
   );
 }
